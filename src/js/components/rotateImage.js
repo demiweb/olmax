c649f436cdf7d41239b9429b75400bf7d37be3de
yurayarosh <yura12323@gmail.com>
2019-05-07 11:05:28 +0300

    upd car spinner

--- src/js/components/rotateImage.js
+++ src/js/components/rotateImage.js
@@ -6,11 +6,13 @@ export default function rotateImage() {
   
   const src = $spinner.data('source');
   const frames = $spinner.data('frame');
+  const width = +$spinner.data('width');
+  const height = +$spinner.data('height');
 
   $spinner.spritespin({
     source: SpriteSpin.sourceArray(src, { frame: frames, digits: 3 }),
-    width   : 480,
-    height  : 327,
+    width   : width,
+    height  : height,
     sense: -1,
     animate: false,
     plugins: [
